4b2d7145dedd77e35ee49f52576a6809cebcb1c4
Jessica M <adajess01@gmail.com>
2021-07-19 04:10:20 -0500

    Updates

---
+++ precache-manifest.7677505accd720c499c0a1670a7cdcc2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2adbf06fd414159236f0",
+    "url": "/static/css/main.dd593077.chunk.css"
+  },
+  {
+    "revision": "2adbf06fd414159236f0",
+    "url": "/static/js/main.2adbf06f.chunk.js"
+  },
+  {
+    "revision": "c3a4c48e6dc48ad6254a",
+    "url": "/static/css/1.bbf4209a.chunk.css"
+  },
+  {
+    "revision": "c3a4c48e6dc48ad6254a",
+    "url": "/static/js/1.c3a4c48e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e9a0ac4ecdedd5f4d2cfa29363e88d7",
+    "url": "/static/media/portfolio_dp.8e9a0ac4.jpg"
+  },
+  {
+    "revision": "4da7829d3cc3b74d6a126a67ec0dc72e",
+    "url": "/static/media/mohamed-mazouz-797941-unsplash.4da7829d.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2d66885d8c755073ca5dd9c2471835a4",
+    "url": "/index.html"
+  }
+];
